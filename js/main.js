ea4073691cff73338696879882d358a253ef50b1
VictorRosenhall <victor.rosenhall@gmail.com>
2025-03-21 14:23:48 +0100

    footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+//storlek (mobil)
+/*@media (max-width: 500px) {
+    header {
+        padding: 10px;
+    }
+
+    h1 {
+        font-size: 24px;
+    }
+
+    .sidebar {
+        width: 250px; 
+        left: -250px;
+    }
+
+    main.shift {
+        margin-left: 250px;
+    }
+}*/
+//Meny
 let layer = document.querySelector(".fade-layer")
 layer.addEventListener("click", showMenu)
 
@@ -25,3 +45,4 @@ function showMenu() {
     let layer = document.querySelector(".fade-layer")
     layer.classList.toggle("visible")
 }*/
+
